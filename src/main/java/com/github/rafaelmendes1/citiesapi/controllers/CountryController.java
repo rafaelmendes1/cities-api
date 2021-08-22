aa5c72af9593015decd9198058a64997d7660915
rafaelmendes1 <rarafafa3245@gmail.com>
2021-08-22 13:23:27 -0300

    Create getCountry endpoint

--- src/main/java/com/github/rafaelmendes1/citiesapi/controllers/CountryController.java
+++ src/main/java/com/github/rafaelmendes1/citiesapi/controllers/CountryController.java
@@ -5,10 +5,14 @@ import com.github.rafaelmendes1.citiesapi.repositories.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/countries")
@@ -17,7 +21,19 @@ public class CountryController {
     @Autowired
     private CountryRepository repository;
 
+    @GetMapping
     public Page<Country> countries(Pageable page) {
         return repository.findAll(page);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Country> getCountry(@PathVariable Long id) {
+        Optional<Country> optional = repository.findById(id);
+
+        if(optional.isPresent()) {
+            return ResponseEntity.ok().body(optional.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
