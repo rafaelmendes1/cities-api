5b109616d392c1d9fb9f4d97d4ea58b0482c69d6
rafaelmendes1 <rarafafa3245@gmail.com>
2021-08-22 16:37:07 -0300

    City controller created

---
+++ src/main/java/com/github/rafaelmendes1/citiesapi/cities/controllers/CityController.java
@@ -0,0 +1,27 @@
+package com.github.rafaelmendes1.citiesapi.cities.controllers;
+
+import com.github.rafaelmendes1.citiesapi.cities.entities.City;
+import com.github.rafaelmendes1.citiesapi.cities.repository.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("cities")
+public class CityController {
+
+    private CityRepository repository;
+
+    @Autowired
+    public CityController(final CityRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public Page<City> cities(final Pageable page) {
+        return repository.findAll(page);
+    }
+}
