0128c0eee3a55c87fd70be4da351efe725437a80
rafaelmendes1 <rarafafa3245@gmail.com>
2021-08-22 16:32:54 -0300

    City entity created

---
+++ src/main/java/com/github/rafaelmendes1/citiesapi/cities/City.java
@@ -0,0 +1,56 @@
+package com.github.rafaelmendes1.citiesapi.cities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cidade")
+public class City {
+
+    @Id
+    private Long id;
+
+    @Column(name = "nome")
+    private String name;
+
+    private Integer uf;
+
+    private Integer ibge;
+
+    @Column(name = "lat_lon")
+    private String geolocation;
+
+    public City() {
+    }
+
+    public City(Long id, String name, Integer uf, Integer ibge,
+                String geolocation) {
+        this.id = id;
+        this.name = name;
+        this.uf = uf;
+        this.ibge = ibge;
+        this.geolocation = geolocation;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getUf() {
+        return uf;
+    }
+
+    public Integer getIbge() {
+        return ibge;
+    }
+
+    public String getGeolocation() {
+        return geolocation;
+    }
+}
